c8d3ebbde623a74836b6380856b82cd3fcd00fd4
Mikko Orispää <mikko.orispaa@gmail.com>
2012-02-03 13:49:25 +0200

    Small fix in zzz.R

--- rlips/R/zzz.R
+++ rlips/R/zzz.R
@@ -3,7 +3,7 @@
 	packageStartupMessage('##\n',
 		'## R Linear Inverse Problem Solver (RLIPS)\n',
 		'## \n',
-		'## Copyright (c) 2011 University of Oulu, Finland\n',
+		'## Copyright (c) 2011-2012 University of Oulu, Finland\n',
 		'## Written by Mikko Orispaa <mikko.orispaa@oulu.fi>\n',
 		'## Licensed under FreeBSD license\n',sep="")
 	
